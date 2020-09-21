584e4406ba56d1d1f3501bceccb932620631f68f
georgematos <georgemattos@gmail.com>
2020-09-21 14:59:25 -0300

    funcoes, declaracao e atribuicao

---
+++ js_funcional/funcoes/1_funcoes_declaracao_e_atribuicao.js
@@ -0,0 +1,21 @@
+// function declaration
+
+function bomDia() {
+  console.log("Bom dia!")
+}
+
+bomDia();
+
+// function expression
+
+const boaTarde = function () { 
+  console.log("Boa tarde!")
+}
+
+boaTarde()
+
+// function expression lambda
+
+const boaNoite = () => console.log("Boa noite!")
+
+boaNoite();
